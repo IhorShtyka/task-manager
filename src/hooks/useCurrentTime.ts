76f333757d71eb8ffdf3543b41d9386371b509af
Ihor Shtyka <ihor.shtyka@gmail.com>
2026-04-30 19:44:38 +0300

    feat: add useCurrentTime hook (refreshes every 60s)

---
+++ src/hooks/useCurrentTime.ts
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+export function useCurrentTime(): Date {
+  const [now, setNow] = useState(() => new Date());
+
+  useEffect(() => {
+    const id = setInterval(() => setNow(new Date()), 60_000);
+    return () => clearInterval(id);
+  }, []);
+
+  return now;
+}
